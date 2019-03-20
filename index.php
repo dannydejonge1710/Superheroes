da95b28c6ee7dfb6a19251d281cc4b029bf691fb
Danny <dannydejonge1710@outlook.com>
2019-03-20 12:10:42 +0100

    Got print r and echo

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'Superhero.php';
+
+$spiderman = new Superhero('Spider-Man', 'Male', 'Spiderfriends', 'With great power comes great responsibility!');
+
+print_r('<pre>'. $spiderman . '</pre>');
+
+$spiderman->sayOneliner();
